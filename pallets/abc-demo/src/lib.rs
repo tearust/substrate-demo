2c0ea8809d89e45f62d5de3d2451cf5c725a10ac
Yang Zhou <deedecho@gmail.com>
2020-11-08 19:22:08 +0800

    fix compilation error

--- pallets/abc-demo/src/lib.rs
+++ pallets/abc-demo/src/lib.rs
@@ -7,6 +7,7 @@ use frame_support::{
 	decl_event, decl_module, decl_storage, decl_error, dispatch,
 	StorageMap, StorageValue, IterableStorageMap, ensure,
 	traits::{Randomness, Currency, ExistenceRequirement, Get, Imbalance}};
+use sp_std::prelude::*;
 
 #[cfg(test)]
 mod mock;
@@ -18,19 +19,21 @@ pub trait Trait: frame_system::Trait {
 	type Event: From<Event<Self>> + Into<<Self as frame_system::Trait>::Event>;
 }
 
+type EmployerAccountId = Vec<u8>;
+
 type ErrandId = Vec<u8>;
 
 type Cid = Vec<u8>;
 
-#[derive(Encode, Decode, Default, Clone, PartialEq, Eq, Debug)]
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
 enum ErrandStatus {
 	Precessing,
 	Done,
 }
 
-#[derive(Encode, Decode, Default, Clone, PartialEq, Eq, Debug)]
-pub struct Errand<AccountId> {
-	account_id: AccountId,
+#[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
+pub struct Errand {
+	account_id: EmployerAccountId,
 	errand_id: ErrandId,
 	description_cid: Cid,
 	status: ErrandStatus,
@@ -40,7 +43,7 @@ pub struct Errand<AccountId> {
 decl_storage! {
 	trait Store for Module<T: Trait> as Abc {
 		Errands get(fn errand):
-			map hasher(twox_64_concat) ErrandId => Option<Errand<T::AccountId>>;
+			map hasher(twox_64_concat) ErrandId => Option<Errand>;
 	}
 }
 
@@ -48,7 +51,7 @@ decl_event!(
 	pub enum Event<T>
 	where
 	AccountId = <T as frame_system::Trait>::AccountId {
-		ErrandSended(AccountId, Errand<AccountId>),
+		ErrandSended(AccountId, Errand),
 	}
 );
 
@@ -71,14 +74,14 @@ decl_module! {
 			description_cid: Cid) -> dispatch::DispatchResult {
 
 			let sender = ensure_signed(origin)?;
-			let errand = Errand{
-				account_id: &sender,
-				errand_id,
+			let errand = Errand {
+				account_id: sender.encode(),
+				errand_id: errand_id.clone(),
 				description_cid,
 				status: ErrandStatus::Precessing,
 				result: Vec::new(),
 			};
-			Errands::<T>::insert(errand_id, errand);
+			Errands::insert(errand_id, errand);
 			// Self::deposit_event(RawEvent::ErrandSended(sender, errand));
 
 			Ok(())
