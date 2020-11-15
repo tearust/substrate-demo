5a5ec64b4a5fd35395ffa569be765a860e38ea7e
Yan Mingzhi <realraindust@gmail.com>
2020-11-15 13:03:22 +0800

    eliminate unit test warnings

--- pallets/abc-demo/src/tests.rs
+++ pallets/abc-demo/src/tests.rs
@@ -1,6 +1,6 @@
 use super::*;
-use crate::{mock::*, Error};
-use frame_support::{assert_noop, assert_ok, debug};
+use crate::mock::*;
+use frame_support::assert_ok;
 
 #[test]
 fn generate_errand_id_test() {
