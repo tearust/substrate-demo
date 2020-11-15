573dc119d555d40a9cca59a694e579b3c2e7fcea
Yan Mingzhi <realraindust@gmail.com>
2020-11-15 12:49:40 +0800

    remove useless template pallet

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -39,9 +39,6 @@ pub use frame_support::{
 	},
 };
 
-/// Import the template pallet.
-pub use pallet_template;
-
 pub use pallet_abc;
 
 /// An index to a block.
@@ -264,11 +261,6 @@ impl pallet_sudo::Trait for Runtime {
 	type Call = Call;
 }
 
-/// Configure the template pallet in pallets/template.
-impl pallet_template::Trait for Runtime {
-	type Event = Event;
-}
-
 impl pallet_abc::Trait for Runtime {
 	type Event = Event;
 }
@@ -354,7 +346,6 @@ construct_runtime!(
 		TransactionPayment: pallet_transaction_payment::{Module, Storage},
 		Sudo: pallet_sudo::{Module, Call, Config<T>, Storage, Event<T>},
 		// Include the custom logic from the template pallet in the runtime.
-		TemplateModule: pallet_template::{Module, Call, Storage, Event<T>},
 		Abc: pallet_abc::{Module, Call, Storage, Event<T>},
 	}
 );
