7dbd502c52daa84df59b6134b5ba6fb7ce07f983
Yan Mingzhi <realraindust@gmail.com>
2020-11-17 21:01:49 +0800

    add send task related logic through http post

--- pallets/abc-demo/src/lib.rs
+++ pallets/abc-demo/src/lib.rs
@@ -9,11 +9,13 @@ use frame_support::{
 };
 use frame_system::{
     ensure_signed,
-    offchain::{AppCrypto, CreateSignedTransaction, SendSignedTransaction, Signer, ForAll},
+    offchain::{AppCrypto, CreateSignedTransaction, ForAll, SendSignedTransaction, Signer},
 };
 use sp_core::crypto::KeyTypeId;
 use sp_io::hashing::blake2_128;
+use sp_runtime::offchain as rt_offchain;
 use sp_std::prelude::*;
+use sp_std::str;
 use uuid::{Builder, Uuid, Variant, Version};
 
 #[cfg(test)]
@@ -22,7 +24,9 @@ mod mock;
 #[cfg(test)]
 mod tests;
 
+pub const SERVICE_BASE_URL: &'static str = "http://localhost:8000";
 pub const KEY_TYPE: KeyTypeId = KeyTypeId(*b"demo");
+pub const TEA_SEND_TASK_TIMEOUT_PERIOD: u64 = 3000;
 
 pub mod crypto {
     use crate::KEY_TYPE;
@@ -40,17 +44,17 @@ pub mod crypto {
     // implemented for ocw-runtime
     impl frame_system::offchain::AppCrypto<MultiSigner, MultiSignature> for AuthId {
         type RuntimeAppPublic = Public;
-        type GenericSignature = sp_core::sr25519::Signature;
         type GenericPublic = sp_core::sr25519::Public;
+        type GenericSignature = sp_core::sr25519::Signature;
     }
 
     // implemented for mock runtime in test
     impl frame_system::offchain::AppCrypto<<Sr25519Signature as Verify>::Signer, Sr25519Signature>
-    for AuthId
+        for AuthId
     {
         type RuntimeAppPublic = Public;
-        type GenericSignature = sp_core::sr25519::Signature;
         type GenericPublic = sp_core::sr25519::Public;
+        type GenericSignature = sp_core::sr25519::Signature;
     }
 }
 
@@ -68,6 +72,14 @@ type EmployerAccountId = Vec<u8>;
 type ErrandId = Vec<u8>;
 
 type Cid = Vec<u8>;
+
+pub struct ErrandService {
+    pub action: Vec<u8>,
+    pub account: Vec<u8>,
+    pub proof_of_delegate: Vec<u8>,
+    pub errand_id: ErrandId,
+    pub description_cid: Vec<u8>,
+}
 
 #[derive(Encode, Decode, Clone, PartialEq, Eq, Debug)]
 enum ErrandStatus {
@@ -108,6 +120,7 @@ decl_error! {
         NoneValue,
         StorageOverflow,
         InsufficientFee,
+        SendErrandTaskError,
     }
 }
 
@@ -205,6 +218,10 @@ impl<T: Trait> Module<T> {
 
         let task_array = Tasks::<T>::get(&current_height);
         for item in task_array.iter() {
+            if let Err(e) = Self::send_task_to_tea_network(&item.1, &item.2) {
+                debug::error!("send_task_to_tea_network error: {:?}", e);
+                continue;
+            }
             Self::init_single_errand_task(&signer, &item.0, &item.1, &item.2);
         }
     }
@@ -221,6 +238,59 @@ impl<T: Trait> Module<T> {
 
         for (_acc, err) in &result {
             debug::error!("init errand {:?} error: {:?}", errand_id, err);
+        }
+    }
+
+    fn send_task_to_tea_network(
+        description_cid: &Cid,
+        errand_id: &ErrandId,
+    ) -> Result<(), Error<T>> {
+        let service = Self::new_errand_service(description_cid, errand_id);
+        let request_url = [
+            SERVICE_BASE_URL,
+            str::from_utf8(&service.action).map_err(|_| Error::<T>::SendErrandTaskError)?,
+            "/",
+            str::from_utf8(&service.account).map_err(|_| Error::<T>::SendErrandTaskError)?,
+            "/",
+            str::from_utf8(&service.errand_id).map_err(|_| Error::<T>::SendErrandTaskError)?,
+            "/",
+            str::from_utf8(&service.proof_of_delegate)
+                .map_err(|_| Error::<T>::SendErrandTaskError)?,
+            "?content=",
+            str::from_utf8(&service.description_cid)
+                .map_err(|_| Error::<T>::SendErrandTaskError)?,
+        ]
+        .concat();
+        let post_body = vec![b""];
+
+        let request = rt_offchain::http::Request::post(&request_url, post_body);
+        let timeout = sp_io::offchain::timestamp().add(rt_offchain::Duration::from_millis(3000));
+        let pending = request
+            .deadline(timeout)
+            .send()
+            .map_err(|_| Error::<T>::SendErrandTaskError)?;
+
+        let response = pending
+            .try_wait(timeout)
+            .map_err(|_| Error::<T>::SendErrandTaskError)?
+            .map_err(|_| Error::<T>::SendErrandTaskError)?;
+
+        if response.code != 200 {
+            debug::error!("Unexpected http request status code: {}", response.code);
+            return Err(<Error<T>>::SendErrandTaskError);
+        }
+
+        Ok(())
+    }
+
+    fn new_errand_service(description_cid: &Cid, errand_id: &ErrandId) -> ErrandService {
+        // todo set real account and proof_of_delegate later
+        ErrandService {
+            action: b"/api/service".to_vec(),
+            account: b"5GBykvvrUz3vwTttgHzUEPdm7G1FND1reBfddQLdiaCbhoMd".to_vec(),
+            proof_of_delegate: b"0x14fd87f46da9cd46750b93ba1aec47dc37ceb132dc97fa2b932bc9938a6cb9306a1fb070926ce9a3ade8ea6b49e51794741de6551daedf6ded090b94691d1c8b".to_vec(),
+            errand_id: errand_id.to_vec(),
+            description_cid: description_cid.to_vec(),
         }
     }
 }
