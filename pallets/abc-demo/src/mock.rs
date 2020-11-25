5f17af78f87e8bc8238cc7eda7902117f1b611e0
jiangzehua <1092431698@qq.com>
2020-11-26 00:53:31 +0800

    modify to let cargo test pass

--- pallets/abc-demo/src/mock.rs
+++ pallets/abc-demo/src/mock.rs
@@ -1,12 +1,13 @@
-use crate::{Module, Trait};
+use crate::*;
 use frame_support::{impl_outer_origin, parameter_types, weights::Weight};
 use frame_system as system;
 use sp_core::H256;
-use sp_runtime::{
-    testing::Header,
-    traits::{BlakeTwo256, IdentityLookup},
-    Perbill,
-};
+use sp_runtime::{testing::{Header, TestXt}, traits::{
+    BlakeTwo256, IdentityLookup, Extrinsic as ExtrinsicT,
+    IdentifyAccount, Verify,
+}, Perbill, MultiSignature};
+use pallet_balances;
+use sp_core::crypto::Ss58Codec;
 
 impl_outer_origin! {
     pub enum Origin for Test {}
@@ -21,9 +22,10 @@ parameter_types! {
     pub const MaximumBlockWeight: Weight = 1024;
     pub const MaximumBlockLength: u32 = 2 * 1024;
     pub const AvailableBlockRatio: Perbill = Perbill::from_percent(75);
+    pub const ExistentialDeposit: u64 = 1;
 }
 
-impl system::Trait for Test {
+impl frame_system::Trait for Test {
     type BaseCallFilter = ();
     type Origin = Origin;
     type Call = ();
@@ -31,7 +33,7 @@ impl system::Trait for Test {
     type BlockNumber = u64;
     type Hash = H256;
     type Hashing = BlakeTwo256;
-    type AccountId = u64;
+    type AccountId = AccountId;
     type Lookup = IdentityLookup<Self::AccountId>;
     type Header = Header;
     type Event = ();
@@ -45,23 +47,79 @@ impl system::Trait for Test {
     type AvailableBlockRatio = AvailableBlockRatio;
     type Version = ();
     type PalletInfo = ();
-    type AccountData = ();
+    type AccountData = pallet_balances::AccountData<u64>;
     type OnNewAccount = ();
     type OnKilledAccount = ();
     type SystemWeightInfo = ();
 }
 
+impl pallet_balances::Trait for Test {
+    type Balance = u64;
+    type DustRemoval = ();
+    type Event = ();
+    type ExistentialDeposit = ExistentialDeposit;
+    type AccountStore = System;
+    type WeightInfo = ();
+    type MaxLocks = ();
+}
+
 impl Trait for Test {
     type Event = ();
+    type AuthorityId = crypto::AuthId;
+    type Call = Call<Test>;
 }
 
 pub type System = system::Module<Test>;
 pub type TemplateModule = Module<Test>;
 
+
+type Extrinsic = TestXt<Call<Test>, ()>;
+type Signature = MultiSignature;
+type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+
+impl frame_system::offchain::SigningTypes for Test {
+    type Public = <Signature as Verify>::Signer;
+    type Signature = Signature;
+}
+
+impl<LocalCall> frame_system::offchain::SendTransactionTypes<LocalCall> for Test where
+    Call<Test>: From<LocalCall>,
+{
+    type OverarchingCall = Call<Test>;
+    type Extrinsic = Extrinsic;
+}
+
+impl<LocalCall> frame_system::offchain::CreateSignedTransaction<LocalCall> for Test where
+    Call<Test>: From<LocalCall>,
+{
+    fn create_transaction<C: frame_system::offchain::AppCrypto<Self::Public, Self::Signature>>(
+        call: Call<Test>,
+        _public: <Signature as Verify>::Signer,
+        _account: AccountId,
+        nonce: u64,
+    ) -> Option<(Call<Test>, <Extrinsic as ExtrinsicT>::SignaturePayload)> {
+        Some((call, (nonce, ())))
+    }
+}
+
+parameter_types! {
+	pub const GracePeriod: u64 = 5;
+	pub const UnsignedInterval: u64 = 128;
+	pub const UnsignedPriority: u64 = 1 << 20;
+}
+
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-    system::GenesisConfig::default()
+    let mut t = system::GenesisConfig::default()
         .build_storage::<Test>()
-        .unwrap()
-        .into()
+        .unwrap();
+
+    let _ = pallet_balances::GenesisConfig::<Test>{
+        balances: vec![
+            (AccountId32::from_string("5G97JLuuT1opraWvfS6Smt4jaAZuyDquP9GjamKVcPC366qU").unwrap(), 1000),
+            (AccountId32::from_string("5EPhGXBymJfrcvT2apo6E9tAm2xpNf8Y77zLxd4zjCBdVJeS").unwrap(), 2000),
+        ],
+    }.assimilate_storage(&mut t).unwrap();
+
+    t.into()
 }
