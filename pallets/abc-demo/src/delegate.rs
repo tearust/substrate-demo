430329efa57df7b1d4d09027b8fb3a2a2b7237f9
Yan Mingzhi <realraindust@gmail.com>
2020-11-27 19:49:19 +0800

    encode be_my_delegate body with protobuf

--- pallets/abc-demo/src/delegate.rs
+++ pallets/abc-demo/src/delegate.rs
@@ -11,6 +11,10 @@ const APPLY_DELEGATE: &'static str = "/api/be_my_delegate";
 const LOCAL_STORAGE_EMPLOYER_KEY_PREFIX: &'static str = "local-storage::employer-";
 const LOCAL_STORAGE_EMPLOYER_LOCK_PREFIX: &'static str = "local-storage::employer-lock-";
 
+mod actor_delegate_proto {
+    include!(concat!(env!("OUT_DIR"), "/actor_delegate.rs"));
+}
+
 #[serde(crate = "alt_serde")]
 #[derive(Encode, Decode, Deserialize)]
 pub struct DelegateInfo {
@@ -26,18 +30,29 @@ pub struct DelegateInfo {
 
 pub fn request_single_delegate(account: AccountId32) {
     let employer = format!("{}", account);
-    let request_url = format!(
-        "{}{}?content={}",
-        SERVICE_BASE_URL, APPLY_DELEGATE, &employer
-    );
+    let proto_msg = actor_delegate_proto::BeMyDelegateRequest {
+        layer1_account: employer.clone(),
+        // todo generate a nonce value
+        nonce: Vec::<u8>::new(),
+    };
+    match encode_protobuf(proto_msg) {
+        Ok(buf) => {
+            let content = base64::encode(buf);
+            let request_url = format!(
+                "{}{}?content={}",
+                SERVICE_BASE_URL, APPLY_DELEGATE, &content
+            );
 
-    match crate::http::http_post(&request_url) {
-        Ok(resp) => {
-            if let Err(e) = parse_delegate_response(resp, &employer) {
-                debug::error!("parse delegate response failed: {}", e)
+            match crate::http::http_post(&request_url) {
+                Ok(resp) => {
+                    if let Err(e) = parse_delegate_response(resp, &employer) {
+                        debug::error!("parse delegate response failed: {}", e)
+                    }
+                }
+                Err(e) => debug::error!("delegate request failed: {}", e),
             }
         }
-        Err(e) => debug::error!("delegate request failed: {}", e),
+        Err(e) => debug::error!("encode protobuf failed: {}", e),
     }
 }
 
@@ -82,4 +97,13 @@ pub fn load_delegate_info(employer: &str) -> anyhow::Result<DelegateInfo> {
         )),
         _ => Err(anyhow::anyhow!("get local storage about {} error")),
     }
+}
+
+pub fn encode_protobuf<T>(protobuf_type: T) -> anyhow::Result<Vec<u8>>
+where
+    T: prost::Message,
+{
+    let mut buf: Vec<u8> = Vec::with_capacity(protobuf_type.encoded_len());
+    protobuf_type.encode(&mut buf)?;
+    Ok(buf)
 }
