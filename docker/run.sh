4eaff757176c5cb67c5f565e46afbf676e7107e5
Yan Mingzhi <realraindust@gmail.com>
2020-12-01 14:53:42 +0800

    update run script

--- docker/run.sh
+++ docker/run.sh
@@ -3,8 +3,14 @@
 Currend_Dir=`pwd`
 echo Current dir: ${Currend_Dir}
 
-Ws_Port="$1"
-echo Used external port: ${Ws_Port}
+Name="$1"
+echo "Use container name with: ${Name}"
 shift
 
-docker run -v ${Currend_Dir}:/build -p ${Ws_Port}:9944 -w /build/target/release -i -t paritytech/ci-linux:staging-1.47.0 ./substrate-demo $@
+Ws_Port="$1"
+echo "Use external web-socket port: ${Ws_Port}"
+shift
+
+docker network create substrate-demo
+
+docker run --name ${Name} --rm --network substrate-demo -v ${Currend_Dir}:/build -p ${Ws_Port}:9944 -w /build/target/release -i -t paritytech/ci-linux:staging-1.47.0 ./substrate-demo $@
